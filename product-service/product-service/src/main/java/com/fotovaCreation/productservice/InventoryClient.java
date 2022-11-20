2342c0f9d2460f7e747f23806913b3de1ee125da
james <monEmail@mail.com>
2022-11-20 21:15:30 +0100

    Ajout de eureka avec feign

--- product-service/product-service/src/main/java/com/fotovaCreation/productservice/InventoryClient.java
+++ product-service/product-service/src/main/java/com/fotovaCreation/productservice/InventoryClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "inventory-client", url = "http://localhost:8003/")
+//@FeignClient(value = "inventory-client", url = "http://localhost:8003/")
+@FeignClient(name = "inventory-service")
 public interface InventoryClient
 {
     @GetMapping("/inventory/{productId}")
--- product-service/product-service/src/main/java/com/fotovaCreation/productservice/PriceClient.java
+++ product-service/product-service/src/main/java/com/fotovaCreation/productservice/PriceClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "price-client", url = "http://localhost:8002/") // le base path
+//@FeignClient(value = "price-client", url = "http://localhost:8002/") // le base path
+@FeignClient(name = "pricing-service")
 public interface PriceClient
 {
     @GetMapping("/price/{productId}")
