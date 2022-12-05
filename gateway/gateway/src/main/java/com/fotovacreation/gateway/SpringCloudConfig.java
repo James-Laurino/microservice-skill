7a88b3615e1b1e1b6d1aa20c20c516a6f35cd577
james <monEmail@mail.com>
2022-12-05 16:51:49 +0100

    ajout des routes

--- gateway/gateway/src/main/java/com/fotovacreation/gateway/SpringCloudConfig.java
+++ gateway/gateway/src/main/java/com/fotovacreation/gateway/SpringCloudConfig.java
@@ -2,11 +2,13 @@ package com.fotovacreation.gateway;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringCloudConfig
 {
+    @Bean
     public RouteLocator buildRouteLocator(RouteLocatorBuilder builder)
     {
         return builder.routes().
